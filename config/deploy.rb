b83ed822b2775104e5351fc416b5be30cf3d9e40
Niko Felger <niko.felger@gmail.com>
2012-01-23 10:08:18 +0000

    Added deployment

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+set :default_shell, "/bin/bash"
+
+$:.unshift(File.expand_path('./lib', '~/.rvm'))
+require "rvm/capistrano"
+set :rvm_ruby_string, '1.8.7'
+set :rvm_type, :user
+
+require 'bundler/capistrano'
+
+set :application, "chat_logs"
+set :repository,  "git@github.com:nfelger/chat-log-viewer.git"
+
+set :scm, :git
+set :user, "nfelger"
+set :deploy_via, :remote_cache
+set :deploy_to, "/home/nfelger/apps/#{application}"
+
+role :app, "of1-dev-nfelger"
+
+namespace :deploy do
+  task :finalize_update do ; end
+  task :start do ; end
+  task :stop do ; end
+  task :restart do ; end
+end
