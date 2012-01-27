f37a1b1209e83244c68a0984bbb72bac84cf50d2
Niko Felger <niko.felger@gmail.com>
2012-01-27 13:01:25 +0000

    Fix for inviting: using the chat name

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,7 +4,7 @@ class ChatsController < ApplicationController
   end
 
   def invite
-    chat_name = Chat.find(params[:id])
+    chat_name = Chat.find(params[:id]).name
     return render :text => "400 Bad Request: username required", :status => 400 unless params[:username].present?
 
     Rype::Chat.new(chat_name).add_members([params[:username]])
